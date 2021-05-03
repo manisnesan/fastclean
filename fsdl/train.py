173448569decebc3934ec819c7c8bcf7d21c01c8
Manikandan Sivanesan <manisnesan@users.noreply.github.com>
2021-05-03 13:18:07 -0400

    Update train script.

--- fsdl/train.py
+++ fsdl/train.py
@@ -2,9 +2,13 @@ from fastai.vision.all import *
 from cleanlab.pruning import get_noise_indices
 from pathlib import Path
 
+import click
+
 # TODO
 # [] Refactor the FILENAME used for loading the data
 
+# Ensuring the script is reproducible
+set_seed(42, reproducible=True)
 
 lbl_dict = dict(
     n01440764='tench',
@@ -42,6 +46,7 @@ def get_dls(df, pref, noice_pct=5, size=128, soft_targets=False):
     dls = dblock.dataloaders(df)
     return dls
 
+
 def train(dls: DataLoaders, filename: str='export.pkl') -> Learner:
     '''
     Train the resnet18 model using the provided dataloaders and save the model using 'filename'
@@ -54,28 +59,58 @@ def train(dls: DataLoaders, filename: str='export.pkl') -> Learner:
 
     return learn
 
+
 def load_data(url=URLs.IMAGENETTE):
+    "Load the data from the desired url and return path downloaded along with the dataframe"
     source = untar_data(url)
     df: pd.DataFrame = pd.read_csv(source/'noisy_imagenette.csv')
     return source, df
 
-def main():
+
+@click.command()
+@click.argument("output_filepath", type=click.Path())
+def main(output_filepath):
     '''
-    Yet to be implemented
+    Evaluation script and write the noisy indices to a separate csv file
     '''
-    # Ensuring the script is reproducible
-    set_seed(42, reproducible=True)
 
     # Loading the data
     source, df = load_data(URLs.IMAGENETTE)
 
     # Default DataLoaders using 5 as noise_percent
-    dls : DataLoaders = get_dls(df, pref=source, size=224)
+    dls: DataLoaders = get_dls(df, pref=source, size=224)
 
-    learn : Learner = train(dls)
-
-    learn.get_preds(ds_idx=0)
+    learn: Learner = train(dls)
 
 
+    # Predict using a single image file
+    # learn = load_learner('export.pkl')
+    # learn.predict('TEST_IMAGE_FILE')
+
+    # Determine the noisy indices on training/validation or an unknown test dataset
+    # Training 
+    train_preds = learn.get_preds(ds_idx=0, with_decoded=True)
+
+    # Add Predictions & Confidence Score
+    decoded_train_preds = get_inverse_transform(L(list(train_preds[2])).map(dls.vocab))
+    
+    # Add confidence & confidenceRange
+    confidence = torch.max(train_preds[0], axis=-1).values
+
+    # Noisy indices from training dataset.
+    train_ordered_label_errors = get_noise_indices(s=train_preds[1].numpy(), #targets
+                             psx=train_preds[0].numpy(),#predictions_prob
+                             sorted_index_method='normalized_margin')
+
+    print("We found {} label errors in the training dataset.".format(len(train_ordered_label_errors)))
+
+    # Actual Noise in the training dataset
+    train_df = df[df.is_valid == False]
+    train_df['predictions'] = np.array(decoded_train_preds)
+    train_df['confidence'] = confidence
+    noisy_train = train_df.iloc[train_ordered_label_errors]
+
+    noisy_train.to_csv(output_filepath)
+    
 if __name__ == '__main__':
     main()
